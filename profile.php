81bcc6fc82f38d4dd641bf2c092074eecbfe7ced
hacker7897iiita <iit2015060@iiita.ac.in>
2018-06-23 20:16:47 +0530

    added update for username and password

--- profile.php
+++ profile.php
@@ -1,12 +1,20 @@
 <?php
 	include 'update.php';
-	if(isset($_POST['chuser'])){
-		$uname  = $_POST['chuname'];
+	if(isset($_POST['chuname'])){
+		$uname  = $_POST['nname'];
 		update($uname,'name');
+	}
+	if(isset($_POST['chpwd'])){
+		$upwd = $_POST['npwd'];
+		update($upwd,'password');
 	}
 ?>
 
 <form method="POST" action="profile.php">
-	<input type="text" name="chuname" placeholder="Change Username"/>
-	<input type="submit" name="chuser" value="Change Username"/>
+	<input type="text" name="nname" placeholder="Change Username"/>
+	<input type="submit" name="chuname" value="Change Username"/>
+</form>
+<form method="POST" action="profile.php">
+	<input type="text" name="npwd" placeholder="Change Password"/>
+	<input type="submit" name="chpwd" value="Change Password"/>
 </form>
