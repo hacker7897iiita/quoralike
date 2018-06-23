56233506b0b4ff5ba6561d50a290df33d1f643eb
hacker7897iiita <iit2015060@iiita.ac.in>
2018-06-24 00:59:35 +0530

    added bootstrap

--- header.php
+++ header.php
@@ -1,28 +1,38 @@
  	<?php session_start();
  	?>
- 	<div class="navbar-fixed">
- 		<nav>
- 			<div class="nav-wrapper">
- 				<a href="feed.php" class="brand-logo">QuoraLike</a>
- 				<ul class="right hide-on-med-and-down">
-					<li><a href="question.php">Ask Question</a></li>
- 					<li><a href="answer.php">Answer</a></li>
- 					<?php
+ 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 		<a class="navbar-brand" href="feed.php">QuoraLike</a>
+ 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+ 			<span class="navbar-toggler-icon"></span>
+ 		</button>
 
- 					if(!isset($_SESSION['login'])){
+ 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+ 			<ul class="navbar-nav mr-auto">
+ 				<li class="nav-item active">
+ 					<a class="nav-link" href="question.php">Ask Question <span class="sr-only">(current)</span></a>
+ 				</li>
+ 				<li class="nav-item">
+ 					<a class="nav-link" href="answer.php">Answer</a>
+ 				</li>
+ 				<?php
+
+ 				if(!isset($_SESSION['login'])){
  						//echo "this is the prob";
- 						echo "<li><a href='index.php'>Login</a></li>";
- 					}
- 					else if($_SESSION['login'] == 1){
+ 					echo "<li class='nav-item'><a class='nav-link' href='index.php'>Login</a></li>";
+ 				}
+ 				else if($_SESSION['login'] == 1){
  						//echo $_SESSION['login'];
- 						echo "<li><a href='logout.php'>Logout</a></li>";
- 					}
- 					else{
+ 					echo "<li class='nav-item'><a class='nav-link' href='logout.php'>Logout</a></li>";
+ 				}
+ 				else{
  						//echo $_SESSION['login'];
- 						echo "<li><a href='index.php'>Login</a></li>";
- 					}
- 					?>
- 				</ul>
- 			</div>
- 		</nav>
- 	</div>
+ 					echo "<li class='nav-item'><a class='nav-link' href='index.php'>Login</a></li>";
+ 				}
+ 				?>
+ 			</ul>
+ 			<form class="form-inline my-2 my-lg-0">
+ 				<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+ 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+ 			</form>
+ 		</div>
+ 	</nav>
--- question.php
+++ question.php
@@ -1,45 +1,78 @@
 <html>
 <head>
-	<!--Import Google Icon Font-->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<!--Import materialize.css-->
-	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
-	<!--Let browser know website is optimized for mobile-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<style media="screen">
-		#popup
-		{
-			margin-right: 25%;
-			margin-left: 25%;
-			margin-top: 80;
-			padding-top: 50;
-			padding-bottom: 50;
-			text-align: center;
-			border-radius: 6px;
-			box-shadow: 0 0 5px 5px #888888;
-		}
-	</style>
+	#popup
+	{
+		margin-right: 25%;
+		margin-left: 25%;
+		margin-top: 80;
+		padding-top: 50;
+		padding-bottom: 50;
+		text-align: center;
+		border-radius: 6px;
+		box-shadow: 0 0 5px 5px #888888;
+	}
+</style>
 </head>
 <body>
-<?php
-include "connect.php";
-include "header.php";
-session_start();
-if(!isset($_SESSION['login']) || $_SESSION['login'] != 1 )
-{
-?>
-<div id="popup"><h1>Login to Ask a Question</h1></div>
-<?php
-}
-else {
-?>
-<textarea resize="none" width"50%" height="300" form="answerForm">Haha</textarea>
-<form action="answer.php" method="POST" id="answerForm">
-<input type="submit" name="answer" value="Upload Answer">
-</form>
-<?php
-}
-?>
+	<?php
+	include "connect.php";
+	include "header.php";
+	session_start();
+
+// code for posing questions
+
+	if(isset($_SESSION['question'])){
+		$qtext = $_SESSION['qtext'];
+		//$sql = "insert into "
+	}
+	// code for posting end here
+
+
+	if(!isset($_SESSION['login']) || $_SESSION['login'] != 1 )
+	{
+		?>
+		<div id="popup"><h1>Login to Ask a Question</h1></div>
+		<?php
+	}
+	else {
+		?>
+		
+		<div class="dropdown">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Dropdown button
+			</button>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="#">Action</a>
+				<a class="dropdown-item" href="#">Another action</a>
+				<a class="dropdown-item" href="#">Something else here</a>
+			</div>
+		</div>
+
+
+
+
+		<textarea resize="none" width"50%" height="300" form="questionform" placeholder="write question" name="qtext"></textarea>
+
+		<form action="question.php" method="POST" id="questionform">
+			<input type="submit" name="question" value="Post"/>
+		</form>
+		<?php
+	}
+	?>
+	<!--JavaScript at end of body for optimized loading-->
+	<!--<script type="text/javascript" src="js/materialize.min.js"></script>-->
+	<select name="carlist" form="questionform">
+		<option value="volvo">Volvo</option>
+		<option value="saab">Saab</option>
+		<option value="opel">Opel</option>
+		<option value="audi">Audi</option>
+	</select>
 </body>
 </html>
