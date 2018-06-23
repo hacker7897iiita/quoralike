e410085d7b3a8ecef1ac4fe97159fa38dffdc5c0
Surya165 <suryakanna165@gmail.com>
2018-06-23 20:33:22 +0530

    added answer page

--- profile.php
+++ profile.php
@@ -1,8 +1,20 @@
 <?php
-	if(isset())
+	include 'update.php';
+	if(isset($_POST['chuname'])){
+		$uname  = $_POST['nname'];
+		update($uname,'name');
+	}
+	if(isset($_POST['chpwd'])){
+		$upwd = $_POST['npwd'];
+		update($upwd,'password');
+	}
 ?>
 
 <form method="POST" action="profile.php">
-	<input type="text" name="changeUsername" placeholder="Change Username"/>
-	<input type="submit" name="chuser" value="Change Username"/>
+	<input type="text" name="nname" placeholder="Change Username"/>
+	<input type="submit" name="chuname" value="Change Username"/>
+</form>
+<form method="POST" action="profile.php">
+	<input type="text" name="npwd" placeholder="Change Password"/>
+	<input type="submit" name="chpwd" value="Change Password"/>
 </form>
---
+++ update.php
@@ -0,0 +1,10 @@
+<?php
+	session_start();
+
+	function update($var,$org){
+		include 'connect.php';
+		$dum = "update users set ".$org." = '".$var."' where email = '".$_SESSION['email']."'";
+		echo $dum;
+		$ret = mysqli_query($con,$dum);
+	}
+?>
